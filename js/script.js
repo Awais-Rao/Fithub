97eecfe291d7d72f1e90adcec11c79a603db516e
Awais Rao <108420364+Awais-Rao@users.noreply.github.com>
2024-08-10 23:02:48 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,46 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 3000,
+    delay: 400,
+    // reset: true
+  });
+  
+  sr.reveal(
+    `.hero__heading, .contact__form`
+  );
+
+  sr.reveal(
+    `.hero__btn_wrapper, .trainer__img_wrapper, .aboutus__img_wrapper`,
+    { origin: "left" }
+  );
+
+  sr.reveal(
+    `.hero__text, .trainer__left_wrapper, .aboutus__content_wrapper`,
+    { origin: "right" }
+  );
+
+  sr.reveal(`.service__card`, {
+    origin: "top",
+    interval: 300,
+    distance: "100px",
+  });
+  
+  sr.reveal(`.glass__card`, {
+    origin: "left",
+    interval: 300,
+    distance: "100px",
+  });
+
+  sr.reveal(`.lesson__card`, {
+    origin: "top",
+    interval: 300,
+    distance: "50px",
+  });
+
+
